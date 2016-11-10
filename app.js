aa0b44e4dc14212cc93180f5e54eea17258c8f94
RodrigoKM <rodrigokoythi@hotmail.com>
2016-11-10 00:39:31 -0200

    Principal

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-(function() {
-'use strict';
+(function () {
+    'use strict';
 
     angular
-        .module('LunchCheck')
+        .module('LunchCheck', [])
         .controller('LunchCheckController', LunchCheckController);
 
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
-        
+        $scope.listLunch = '';
+        $scope.message = '';
+
+        $scope.CheckIfTooMuch = function () {
+            if (isEmpty($scope.listLunch)) {
+                $scope.message = 'Please enter data first';
+            } else if(splitString($scope.listLunch).length <= 3){
+                $scope.message = 'Enjoy!';
+            } else {
+                $scope.message = 'Too much!';
+            }
+        }
     }
+
+    function isEmpty(list) {
+        return list === '';
+    }
+
+    function splitString(list) {
+        return list.split(',').filter(function(i){return i.trim().length != 0 });
+    }
+
 })();
